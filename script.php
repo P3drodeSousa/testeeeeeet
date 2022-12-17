abfb555fa909e1ad6e86152aa8ed8d148e7448f6
unknown <pedro_de_sousa@outlook.com>
2022-12-18 00:45:10 +0100

    test

--- script.php
+++ script.php
@@ -6,22 +6,20 @@ $LOCAL_ROOT         = "/api";
 $LOCAL_REPO_NAME    = "REPO_NAME";
 $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "git@github.com:P3drodeSousa/testeeeeeet.git";
-$BRANCH             = "main";
+$BRANCH             = "master";
 
-if ($_POST['payload']) {
-    // Only respond to POST requests from Github
 
-    if (file_exists($LOCAL_ROOT)) {
 
-        // If there is already a repo, just run a git pull to grab the latest changes
-        shell_exec("cd {$LOCAL_ROOT} && git pull");
-        echo (shell_exec("git status"));
-        die("done " . mktime());
-    } else {
+if (file_exists($LOCAL_ROOT)) {
 
-        // If the repo does not exist, then clone it into the parent directory
-        shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
+    // If there is already a repo, just run a git pull to grab the latest changes
+    shell_exec("cd {$LOCAL_ROOT} && git pull");
+    echo (shell_exec("git status"));
+    die("done " . mktime());
+} else {
 
-        die("done " . mktime());
-    }
+    // If the repo does not exist, then clone it into the parent directory
+    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
+
+    die("done " . mktime());
 }
