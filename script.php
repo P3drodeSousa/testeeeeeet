70cf1894c1b9f8a9c406af27b14230baabe805bf
unknown <pedro_de_sousa@outlook.com>
2022-12-18 00:50:15 +0100

    test

--- script.php
+++ script.php
@@ -15,11 +15,11 @@ if (file_exists($LOCAL_ROOT)) {
     // If there is already a repo, just run a git pull to grab the latest changes
     shell_exec("cd {$LOCAL_ROOT} && git pull");
     echo (shell_exec("git status"));
-    die("done " . mktime());
+    die("done ");
 } else {
 
     // If the repo does not exist, then clone it into the parent directory
     shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-
-    die("done " . mktime());
+    echo (shell_exec("git status"));
+    die("done ");
 }
